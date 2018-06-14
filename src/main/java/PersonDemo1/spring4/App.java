0f6e1ed88e50225e00e26fe48335c7690d32b70a
lnb <921894@gmail.com>
2018-06-14 18:21:29 +0800

    init

---
+++ src/main/java/PersonDemo1/spring4/App.java
@@ -0,0 +1,13 @@
+package PersonDemo1.spring4;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
